65cfef75ffd057fb05d99d52ab6e200872250e81
liucheng.c <liucheng.c@bytedance.com>
2022-09-23 00:55:55 +0800

    update model

--- model/TreeDir.go
+++ model/TreeDir.go
@@ -1,11 +1,11 @@
 package model
 
 type TreeDir struct {
-	Label       string
-	Children    []*TreeDir
-	RPath       string
-	APath       string
-	IsDir       bool
-	IsHide      bool
-	LastModTime string
+	Label       string     `json:"label,omitempty"`
+	Children    []*TreeDir `json:"children,omitempty"`
+	RPath       string     `json:"r_path,omitempty"`
+	APath       string     `json:"-"`
+	IsDir       bool       `json:"is_dir,omitempty"`
+	IsHide      bool       `json:"is_hide,omitempty"`
+	LastModTime string     `json:"last_mod_time,omitempty"`
 }
