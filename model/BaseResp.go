4267e47e4f3b1e80be57a643c4e47c1c47cdd7f7
liucheng.c <liucheng.c@bytedance.com>
2022-09-24 11:39:59 +0800

    fix int omitempty

--- model/BaseResp.go
+++ model/BaseResp.go
@@ -1,7 +1,7 @@
 package model
 
 type BaseResp struct {
-	Code int         `json:"code,omitempty"`
+	Code int         `json:"code"` // if code have omitempty tag, it would be ignored when it is assigned to 0
 	Msg  string      `json:"msg,omitempty"`
 	Data interface{} `json:"data,omitempty"`
 }
